970c58584cb7110a6b77da608516a9d0e7664152
jie.fu <fujie1988@gmail.com>
2016-10-12 22:03:07 +0800

    update

--- twoandhalf_spider/start.sh
+++ twoandhalf_spider/start.sh
@@ -1,1 +1,3 @@
-scrapy crawl $1 --logfile $1.log -L INFO -o $1.jl -s JOBDIR=tmp/$1
+ROOT=/data/crawlers/$1
+mkdir -p $ROOT
+scrapy crawl $1 --logfile $ROOT/$1.log -L INFO -o $ROOT/$1.jl -s JOBDIR=$ROOT/tmp/$1
